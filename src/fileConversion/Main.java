8de3336b514e9496a70be3e639cb0b4c5b881a51
irishandyesiam <nathan.irish@gmail.com>
2023-04-11 15:42:14 -0500

    Sample two.

--- src/fileConversion/Main.java
+++ src/fileConversion/Main.java
@@ -1,60 +1,100 @@
 package fileConversion;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 
-public class Main {
+// Class to represent a member
+class Member {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
 
-    public static void main(String[] args) {
-        String inputFile = "Members.txt";
-        String outputFilePrefix = "_outputFile.csv";
+    public Member(String id, String firstName, String lastName, String address, String city, String state, String zip) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
 
-        try {
-            // Read input file
-            BufferedReader br = new BufferedReader(new FileReader(inputFile));
+    // Getters and Setters
+    // ...
+
+    // toString method to convert member object to CSV string
+    @Override
+    public String toString() {
+        return id + "," + firstName + "," + lastName + "," + address + "," + city + "," + zip;
+    }
+}
+
+// Interface to export members to a CSV file
+interface MemberExporter {
+    void exportMembers(List<Member> members, String fileName) throws IOException;
+}
+
+// Interface to import members from a fixed-column format file
+interface MemberImporter {
+    List<Member> importMembers(String fileName) throws IOException;
+}
+
+// Implementation of MemberExporter interface
+class MemberExporterImpl implements MemberExporter {
+    @Override
+    public void exportMembers(List<Member> members, String fileName) throws IOException {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(fileName))) {
+            for (Member member : members) {
+                writer.println(member.toString());
+            }
+        }
+    }
+}
+
+// Implementation of MemberImporter interface
+class MemberImporterImpl implements MemberImporter {
+    @Override
+    public List<Member> importMembers(String fileName) throws IOException {
+        List<Member> members = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
-            Map<String, FileWriter> outputFiles = new HashMap<>();
-            Set<String> uniqueMembers = new HashSet<>();
-            while ((line = br.readLine()) != null) {
+            while ((line = reader.readLine()) != null) {
                 String id = line.substring(0, 12).trim();
                 String lastName = line.substring(12, 37).trim();
                 String firstName = line.substring(37, 62).trim();
                 String address = line.substring(62, 92).trim();
                 String city = line.substring(92, 112).trim();
                 String state = line.substring(112, 116).trim();
-                String zip = line.substring(116, 121).trim();
-                String memberData = id + "," + firstName + "," + lastName + "," + address + "," + city + "," + zip;
-
-                // Skip duplicate members
-                if (uniqueMembers.contains(memberData)) {
-                    continue;
-                }
-                uniqueMembers.add(memberData);
-
-                // Create output file for each state if not exists
-                if (!outputFiles.containsKey(state)) {
-                    FileWriter fw = new FileWriter(state + outputFilePrefix);
-                    outputFiles.put(state, fw);
-                }
-
-                // Write member data to respective output file
-                FileWriter fw = outputFiles.get(state);
-                fw.write(memberData + "\n");
+                String zip = line.substring(116).trim();
+                Member member = new Member(id, firstName, lastName, address, city, state, zip);
+                members.add(member);
             }
+        }
+        return members;
+    }
+}
 
-            // Close all output files
-            for (FileWriter fw : outputFiles.values()) {
-                fw.close();
+// Main class to perform the conversion
+public class Main {
+    public static void main(String[] args) throws IOException {
+        MemberImporter memberImporter = new MemberImporterImpl();
+        List<Member> members = memberImporter.importMembers("Members.txt");
+        Map<String, List<Member>> membersByState = new HashMap<>();
+        for (Member member : members) {
+            String state = member.getState();
+            if (!membersByState.containsKey(state)) {
+                membersByState.put(state, new ArrayList<>());
             }
-
-            System.out.println("Conversion completed successfully!");
-        } catch (IOException e) {
-            System.err.println("Error occurred during file I/O: " + e.getMessage());
+            membersByState.get(state).add(member);
+        }
+        MemberExporter memberExporter = new MemberExporterImpl();
+        for (String state : membersByState.keySet()) {
+            String fileName = state + "_outputFile.csv";
+            memberExporter.exportMembers(membersByState.get(state), fileName);
         }
     }
 }
+
