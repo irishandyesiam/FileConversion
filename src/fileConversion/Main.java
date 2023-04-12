08f7c34f21e86a9de75ad6206d00e3de00ba4fa8
irishandyesiam <nathan.irish@gmail.com>
2023-04-11 22:26:06 -0500

    Parse line to extract key-value pairs.

--- src/fileConversion/Main.java
+++ src/fileConversion/Main.java
@@ -7,13 +7,22 @@ public class Main {
     	String inputFile = "C:\\Users\\User\\Desktop\\Coding Aids\\input.txt";
     	String outputFile = "output.txt";
     	
-        //Read the input text file line by line using a FileReader or a BufferedReader
+        //Step 1: Read the input text file line by line using a FileReader or a BufferedReader
     	try {
 			BufferedReader br = new BufferedReader(new FileReader(inputFile));
 			String line;
-			while((line = br.readLine()) != null)
-				System.out.println(line);
-			br.close();
+			while ((line = br.readLine()) != null) {
+				// Step 2: Parse line to extract key-value pairs
+				String inputLine = line; // Replace with the actual line read from the input file
+				Map<String, String> keyValuePairs = new HashMap<>(); // Use a suitable data structure to store key-value pairs
+				String[] keyValue = inputLine.split(":"); // Split line using colon as delimiter
+				if (keyValue.length == 2) {
+				    String key = keyValue[0].trim(); // Extract key and trim leading/trailing spaces
+				    String value = keyValue[1].trim(); // Extract value and trim leading/trailing spaces
+				    keyValuePairs.put(key, value); // Store key-value pair in the data structure
+				}
+			}	
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
