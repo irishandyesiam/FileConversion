a71579864a4c6610424c3687446e9ba79c73d0b7
irishandyesiam <nathan.irish@gmail.com>
2023-04-12 10:49:47 -0500

    When encounter empty line, add the stored key-value pairs directly to a CSV file.

--- src/fileConversion/Main.java
+++ src/fileConversion/Main.java
@@ -5,25 +5,37 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
     	String inputFile = "C:\\Users\\User\\Desktop\\Coding Aids\\input.txt";
-    	String outputFile = "output.txt";
+    	String outputFile = "new_output.txt";
     	
-        //Step 1: Read the input text file line by line using a FileReader or a BufferedReader
     	try {
 			BufferedReader br = new BufferedReader(new FileReader(inputFile));
 			String line;
 			while ((line = br.readLine()) != null) {
-				// Step 2: Parse line to extract key-value pairs
-				String inputLine = line; // Replace with the actual line read from the input file
-				Map<String, String> keyValuePairs = new HashMap<>(); // Use a suitable data structure to store key-value pairs
-				String[] keyValue = inputLine.split(":"); // Split line using colon as delimiter
+				String inputLine = line; 
+				Map<String, String> keyValuePairs = new HashMap<>(); 
+				String[] keyValue = inputLine.split(":"); 
 				if (keyValue.length == 2) {
-				    String key = keyValue[0].trim(); // Extract key and trim leading/trailing spaces
-				    String value = keyValue[1].trim(); // Extract value and trim leading/trailing spaces
-				    keyValuePairs.put(key, value); // Store key-value pair in the data structure
+				    String key = keyValue[0].trim(); 
+				    String value = keyValue[1].trim(); 
+				    keyValuePairs.put(key, value);
 				}
+			BufferedWriter bwr = new BufferedWriter(new FileWriter(outputFile));
+			bwr.write("Key,Value");
+			bwr.newLine();
+			for (Map.Entry<String, String> entry : keyValuePairs.entrySet()) {
+				String key = entry.getKey();
+				String value = entry.getValue();
+				String csvRow = key + "," + value;
+				bwr.write(csvRow);
+				bwr.newLine();
+			}
+			
+			System.out.println("Successfully wrote key-value pairs to CSV file: " + outputFile);
+			
 			}	
 
 		} catch (IOException e) {
+			System.err.println("Error writing key-value pairs to CSV file: " + e.getMessage());
 			e.printStackTrace();
 		}
     }
